6bb15b327d66d8594e70a85a72aaad3778a4a09d
davidanderson281 <david.anderson281@yahoo.co.uk>
2024-04-04 22:38:07 +0100

    Macbook changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavigationComponent } from './component/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  cols: number = 2;
+    private windowWidth: number = window.innerWidth;
+    // change cols variable dynamically based on screen size
+    ngOnInit(): void {
+      window.addEventListener('resize', () => {
+        if (window.innerWidth < 600) {
+          this.cols = 1;
+          console.log("Small screen");
+        } else {
+          this.cols = 2;
+          console.log("Large screen");
+        }
+      });
+    }
 }
