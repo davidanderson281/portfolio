1762967f1a198b9abb4a15debcf3144111a424b9
David Anderson <43776319+davidanderson281@users.noreply.github.com>
2024-04-15 11:38:38 +0100

    Multiple UI changes, starting template for further work

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, MatCardModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tiles: any[] = [
+    {title: 'Skills', content: 'Here are my skills and knowledge'},
+    {title: 'Experiences', content: 'Here are my experiences'},
+    {title: 'Projects', content: 'Here are my projects'}
+  ];
+
+  isMobile(): boolean {
+    const toCheck = window.innerWidth;
+    return toCheck <= 768;
+  }
 
 }
